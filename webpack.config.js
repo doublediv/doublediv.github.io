d74c1f06da10e1bf67997e4fd8ffdf49de5083ad
zxc <zhangcanxiao@163.com>
2019-06-06 21:05:50 +0800

    '头部'

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,94 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const package = require('./package.json');
+function resolve(dir) {
+    return path.join(__dirname, '..', dir)
+}
 
 module.exports = {
     entry: {
-        app: "./src/index.js",
+        app: "./src/main.js",
     },
     devtool: 'inline-source-map',
+    resolve: {
+        extensions: ['.js', '.json'],
+        alias: {
+            '@': resolve('src'),
+        }
+    },
     devServer: {
-        contentBase: './dist',
-        hot: true
+        contentBase: path.resolve(__dirname, './dist'),
+        host: '0.0.0.0',
+        port: 3737,
+        hot: true,
+        publicPath: '/'
+    },
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "static/js/[name].[hash:5].js",
+        chunkFilename: "static/js/[id].[hash:5].js",
+        publicPath: "./"
     },
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            title: package.name
+            filename: path.resolve(__dirname, './dist/index.html'),
+            template: './src/pages/index.html',
+            favicon: '',
+            minify: {
+                minimize: true,                 //打包为最小值
+                removeAttributeQuotes: true,    //去除引号
+                removeComments: true,           //去除注释
+                collapseWhitespace: true,       //去除空格
+                minifyCSS: true,                //压缩html内css
+                minifyJS: true,                 //压缩html内js
+                removeEmptyElements: true,      //清除内存为空的元素
+            },
+            hash: true
+        }),
+        new MiniCssExtractPlugin({
+            filename: "static/css/layout.[hash:5].css",
+            chunkFilename: "static/css/[id].[hash:5].css",
+            publicPath: '../../'
         }),
         new webpack.NamedModulesPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
+        new webpack.HotModuleReplacementPlugin()
     ],
-    output: {
-        filename: "[name].bundle.js",
-        path: path.resolve(__dirname, "dist"),
-        publicPath: '/'
-    },
     module: {
         rules: [
             {
+                test: /\.html$/,
+                loader: "underscore-template-loader"
+            },
+            {
                 test: /\.css$/,
                 use: [
-                    "style-loader",
-                    "css-loader"
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '../'
+                        }
+                    },
+                    "css-loader",
+                    "postcss-loader"
+
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '../'
+                        }
+                    },
+                    'css-loader',
+                    "postcss-loader",
+                    'less-loader'
                 ]
             },
             {
