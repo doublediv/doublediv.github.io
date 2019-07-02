82e7b43ba4799489bbefde0434b4f3004a74566b
zxc <zhangcanxiao@163.com>
2019-07-02 19:23:12 +0800

    修改打包配置

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,15 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 function resolve(dir) {
     return path.join(__dirname, '..', dir)
 }
 
-module.exports = {
+const isDev = process.env.NODE_ENV == 'dev';
+
+let config = {
     entry: {
         app: "./src/main.js",
     },
@@ -19,20 +22,17 @@ module.exports = {
             '@src': resolve('src'),
         }
     },
-    devServer: {
-        contentBase: path.resolve(__dirname, './dist'),
-        host: '0.0.0.0',
-        port: 3737,
-        hot: true,
-        publicPath: '/'
-    },
     output: {
         path: path.resolve(__dirname, "./dist"),
         filename: "static/js/[name].[hash:5].js",
-        chunkFilename: "static/js/[id].[hash:5].js",
         publicPath: "./"
     },
     plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: isDev ? '"development"' : '"production"'
+            }
+        }),
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             filename: path.resolve(__dirname, './dist/index.html'),
@@ -40,7 +40,7 @@ module.exports = {
             favicon: '',
             minify: {
                 minimize: true,                 //打包为最小值
-                removeAttributeQuotes: true,    //去除引号
+                // removeAttributeQuotes: true,    //去除引号
                 removeComments: true,           //去除注释
                 // collapseWhitespace: true,       //去除空格
                 minifyCSS: true,                //压缩html内css
@@ -54,8 +54,12 @@ module.exports = {
             chunkFilename: "static/css/[id].[hash:5].css",
             publicPath: '../'
         }),
-        new webpack.NamedModulesPlugin(),
-        new webpack.HotModuleReplacementPlugin()
+        new CopyPlugin([
+            {
+                from: path.resolve(__dirname, "./static"),
+                to: './static'
+            },
+        ]),
     ],
     module: {
         rules: [
@@ -73,8 +77,12 @@ module.exports = {
                         }
                     },
                     "css-loader",
-                    "postcss-loader"
-
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
                 ]
             },
             {
@@ -87,7 +95,12 @@ module.exports = {
                         }
                     },
                     'css-loader',
-                    "postcss-loader",
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true,
+                        }
+                    },
                     'less-loader'
                 ]
             },
@@ -114,3 +127,35 @@ module.exports = {
         ]
     }
 };
+if (isDev) {
+    config.devtool = "#cheap-module-eval-source-map";
+    config.devServer = {
+        contentBase: path.resolve(__dirname, './dist'),
+        host: '0.0.0.0',
+        port: 3737,
+        hot: true,
+        publicPath: '/'
+    }
+    config.plugins.push(
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin()
+    )
+} else {
+    config.output.filename = 'static/js/[name].[chunkhash:5].js';
+    new webpack.optimize.CommonsChunkPlugin({
+        name: 'runtime'
+    })
+    // config.optimization = {
+    //     splitChunks: {
+    //         cacheGroups: {
+    //             commons: {
+    //                 name: "commons",
+    //                 chunks: "initial",
+    //                 minChunks: 2
+    //             }
+    //         }
+    //     }
+    // }
+}
+
+module.exports = config
